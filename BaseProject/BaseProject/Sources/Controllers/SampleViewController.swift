7b0cab73ffde7447922748e1afcf7c10e476916d
Taehyeon Kim <taehyk528@gmail.com>
2021-08-13 02:11:41 +0900

    Initial commit

---
+++ BaseProject/BaseProject/Sources/Controllers/SampleViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SampleViewController.swift
+//  BaseProject
+//
+//  Created by taehy.k on 2021/07/31.
+//
+
+import UIKit
+
+class SampleViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func setupUI() {
+        super.setupUI()
+        print("SampleViewController setupUI() Override.")
+    }
+}
