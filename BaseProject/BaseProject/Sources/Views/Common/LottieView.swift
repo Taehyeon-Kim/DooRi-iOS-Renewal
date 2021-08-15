d29c0fbafc7be766c855a96f92dd013df370225e
김태현 <taehyk528@gmail.com>
2021-08-15 21:54:03 +0900

    [FEAT] Lottie View 모듈화 - 커스텀뷰로 분리

---
+++ BaseProject/BaseProject/Sources/Views/Common/LottieView.swift
@@ -0,0 +1,39 @@
+//
+//  LottieView.swift
+//  BaseProject
+//
+//  Created by taehy.k on 2021/08/15.
+//
+
+import UIKit
+
+import Lottie
+
+class LottieView: UIView {
+    
+    var animationView = AnimationView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        animationView.frame = frame
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(named: String, frame: CGRect) {
+        self.init(frame: frame)
+        animationView.frame = self.bounds
+        animationView.animation = Animation.named(named)
+        animationView.contentMode = .scaleAspectFit
+        animationView.isHidden = true
+        animationView.stop()
+        addSubview(animationView)
+    }
+    
+    public func startAnimation() {
+        animationView.isHidden = false
+        animationView.play()
+    }
+}
